7409878cf10e7edd40d272f43af0388a0c01f877
fujitashuichi <fujitashuu14@gmail.com>
2026-04-11 15:29:26 +0900

    wip: tailwind設定

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,8 +3,11 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div className="flex bg-amber-200 w-full h-20 justify-center">
+      <h1 className="text-2xl">
+        Test Tailwind
+      </h1>
+    </div>
   )
 }
 
